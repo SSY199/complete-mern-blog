9923b21acff6c6f0e517ec7be6a198b18bf08b50
SSY199 <s.s.y.9321423@gmail.com>
2025-02-15 10:32:46 +0530

    Addedd Sigin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/errorHandler.js'
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
   if (!username || !email || !password) {
-    return next(errorHandler(400, 'All fields are required'));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -13,12 +14,46 @@ export const signup = async (req, res, next) => {
   const newUser = new User({
     username,
     email,
-    password: hashedPassword
+    password: hashedPassword,
   });
 
   try {
     await newUser.save();
-    return res.status(201).json({ message: 'User created successfully' });
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return res.status(404).json({ message: "Invalid credentials " });
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
