688fa095e4b64df55b66f34a145b82c367f5460e
SSY199 <s.s.y.9321423@gmail.com>
2025-02-15 08:25:14 +0530

    Created UserSchema

---
+++ api/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20
+  }, 
+  email: {
+    type: 'string',
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  password: {
+    type: 'string',
+    required: true,
+    minlength: 5
+  },
+  profilePicture: {
+    type: 'string',
+    default: 'https://cdn.pixabay.com/photo/2021/11/24/05/19/user-6820232_1280.png'
+  },
+  isAdmin: {
+    type: 'boolean',
+    default: false,
+  },
+},
+{
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
